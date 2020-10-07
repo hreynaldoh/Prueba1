028ccfef056281763cebf8aa6bbb3aa5535c67de
Reynaldo Hernández Hernández <hreynaldoh@gmail.com>
2020-10-07 15:58:15 -0500

    1.1

---
+++ src/main/java/VentanaPrueba.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author hreynaldoh
+ */
+import javax.swing.*;
+
+public class VentanaPrueba {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Mi ventana");
+        f.setSize(400, 300);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
